03d81abb056e5b740f5ae52d5d134c8bbb19ed49
Ritikg269 <115003852+Ritikg269@users.noreply.github.com>
2024-11-06 16:27:44 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" ; // using this line then all code will execute in newer version of JS.
+
+// alert (4+4); // using node.js not working in browser 
+
+// Diff Primitive Datatypes in JS
+
+// number  => 2 to power 53
+// bigint
+// string ""
+// boolean => true / false 
+// null => standalone value 
+// undefined 
+// symbol
+
+
+// object 
+
+console.log(typeof undefined);   // undefined 
+
+console.log(typeof null);       // object type (problem of JS)
