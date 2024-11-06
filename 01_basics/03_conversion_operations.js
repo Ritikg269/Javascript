935565df4cd3c0abbc0fa6384c907fc14efc5932
Ritikg269 <115003852+Ritikg269@users.noreply.github.com>
2024-11-06 18:02:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,17 @@
+let score = undefined;
+
+console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInnumber = Number(score);
+
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+
+
+/*
+"33" => 33
+"33abc" => NaN
+null => 0
+undefined => NaN
+*/
