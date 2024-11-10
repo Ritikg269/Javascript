727129ead8ae2ab17a0e797a57e9a26a966308f0
Ritikg269 <115003852+Ritikg269@users.noreply.github.com>
2024-11-10 07:58:46 +0000

    Datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+/*Javascript => JavaScript is dynamically typed, meaning that type checking is done at runtime, rather than during compilation.
+Some other languages that use dynamic typing include Python and Ruby. 
+
+
+Basically JS divided 2 category of datatypes.
+1 - Primitive data 
+2- Non-primitive data
+
+=> Primitive datatypes (call by value)- 7 datatypes are primitive
+String, Number, Boolean , Null , Undefined, Symbol, BigINt
+
+
+
+=> Reference or Non-primitive datatypes (call by reference 
+Array, Object and Functions)
+
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  => function object
+       Object  =>  object
+*/
