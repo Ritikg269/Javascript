e859aaff221fffe72d9d53f959287d502ff4a74b
Ritikg269 <115003852+Ritikg269@users.noreply.github.com>
2024-12-05 18:51:25 +0000

    string basis

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,8 @@
+const name = "Ritik";
+const score = 100;
+
+console.log(name + score + " best");  // This is string concatenation method but we dont use this nowdays bcz this is little older version.
+
+console.log(`Hello my name is ${name} and my score is ${score} and im the best`); //This method is known as String Interpolation , in which we create the placeholder and we can assign value in them.
+
+
